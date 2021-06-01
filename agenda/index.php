e8e5d0c411b79fde2023934abde2f89f591d765a
juancarlosMJ <mendez_30_yo@hotmail.com>
2021-06-01 06:49:26 -0500

    'practicas'

---
+++ agenda/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda</title>
+	<link rel="icon" type="jj/png" href="public/img/jj.png" style="background-color: tan;">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<?php require_once "dependencias.php"; ?>
+</head>
+<body>
+	<div class="container">
+		<?php require_once "menu.php"; ?>
+
+		<div class="jumbotron">
+			<h1 class="display-4">Agenda de contactos con PHP y MySQL</h1>
+			<hr>
+
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="public/img/j.png" class="img-fluid" alt="Responsive image">
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-hover">
+								<tr>
+									<th>
+										Agenda 2021
+									</th>
+								</tr>
+								<tr>
+									<th>
+										por juan 
+									</th>
+								</tr>
+								<tr>
+									<th>
+										Hecho con php y mysql
+									</th>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr class="my-4">
+			<p>
+			<hr>
+				Agenda de contactos con php y mysql 
+			</p>
+			
+		</div>
+
+	</div>
+
+	
+</body>
+</html>
---
+++ agenda/menu.php
@@ -0,0 +1,36 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <a class="navbar-brand" href="index.php">
+    <i class="far fa-address-card fa-5x"></i></span>
+  </a>
+  <button class="navbar-toggler" type="button"
+   data-toggle="collapse"
+   data-target="#navbarSupportedContent" 
+   aria-controls="navbarSupportedContent"
+    aria-expanded="false" 
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <span class="fas fa-house-user"></span> Inicio <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="contactos.php">
+          <span class="fas fa-id-card-alt"></span> Contactos
+        </a>
+      </li>
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="categorias.php">
+          <span class="fas fa-layer-group"></span> Categorias
+        </a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
